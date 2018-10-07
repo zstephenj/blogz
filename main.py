767a92b1cb087db8be7ba06c40530a64c61f14ba
Stephen Zhang <zhangstephenj@gmail.com>
2018-10-07 00:24:50 -0500

    Reconfigured for database, Completed app functionality

--- main.py
+++ main.py
@@ -16,21 +16,37 @@ class Bpost(db.Model):
         self.blog_title = blog_title
         self.blog_content = blog_content
 
-@app.route("/newpost", methods=['POST'])
-def validate_new_post():
-    blog_title = request.form['blog_title']
-    blog_body = request.form['blog_body']
-    title_error = ''
-    body_error = ''
-    return render_template("newpost.html", title="Create New Post", )
-
-@app.route("/newpost")
+@app.route("/newpost", methods=['POST', 'GET'])
 def new_post():
-    return render_template("newpost.html", title="Create New Post")
+    if request.method == 'POST':
+        blog_title = request.form['blog_title']
+        blog_body = request.form['blog_body']
+        title_error = ''
+        body_error = ''
+        if not blog_title:
+            title_error = 'Post title cannot be empty.'
+        if not blog_body:
+            body_error = 'Post body cannot be empty.'   
+        if not title_error and not body_error:
+            new_post = Bpost(blog_title, blog_body)
+            db.session.add(new_post)
+            db.session.commit()
+            blog_id = new_post.id 
+            return redirect('/blog?id={0}'.format(blog_id))
+        else:
+            return render_template("newpost.html", title="Create New Post", blog_title=blog_title, blog_body=blog_body, title_error=title_error, body_error = body_error)
+    else:
+        return render_template("newpost.html", title="Create New Post")
 
 @app.route("/blog")
-def main_blog():
-    return render_template("blog.html", title="Demo Blog")
+def blog():
+    id = request.args.get('id')
+    if not id == None:
+        content = Bpost.query.filter_by(id=id).all()
+        return render_template("blog.html", title="Demo Blog", content=content) 
+    else:
+        content = Bpost.query.all()
+        return render_template("blog.html", title="Demo Blog", content=content)
 
 if __name__ == '__main__':
     app.run ()
